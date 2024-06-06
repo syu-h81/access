1db7f5cde9b0aafe8d230d89e6114baacd3bfe25
Danjo Syu <>
2024-06-06 20:05:13 +0900

    css修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,13 @@ $(function(){
   $('.announce-toggle').on('click', function() {
     $('.toolboard').slideToggle('.open');
   })
+
+  //条件抽出のポップアップ表示
+  $('.search-extract-btn').on('click', function() {
+    $('.popup').toggle('.open');
+  })
+
+  $('.popup-content-close-box').on('click', function() {
+    $('.popup').toggle('oepn');
+  })
 });
