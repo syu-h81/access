9b4183eccbdd164610e5ab1453a7071fcf82839a
Danjo Syu <>
2024-06-07 10:06:09 +0900

    条件抽出中のポップアップ実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,18 @@ $(function(){
   //条件抽出のポップアップ表示
   $('.search-extract-btn').on('click', function() {
     $('.popup').toggle('.open');
+    $('.popup-content').removeClass('hidden');
+    $('.popup-extract-logo').removeClass('open');
   })
 
   $('.popup-content-close-box').on('click', function() {
-    $('.popup').toggle('oepn');
+    $('.popup').toggle('open');
+  })
+
+  //条件抽出中のロードポップアップ表示
+  $('.popup-content-result-btn').on('click', function() {
+    $('.popup-content').addClass('hidden');
+    $('.popup-extract-logo').addClass('open');
+    $('.popup').fadeOut(4000);
   })
 });
