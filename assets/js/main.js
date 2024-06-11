ad0b096db5b465abc1931461f94f8c864af5976d
Danjo Syu <>
2024-06-11 22:21:52 +0900

    ローディング画面の実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,12 @@ $(function(){
     $('.popup-extract-logo').addClass('open');
     $('.popup').fadeOut(4000);
   })
+  //ローディング機能の処理
+  $('#seller-btn, #asin-btn').on('click', function() {
+    $('.nav-infoacq-heading-stand').hide();
+    $('.loading').addClass('loading-open');
+    $('.nav-infoacq-heading-name').addClass('loading-open')
+    $('.nav-infoacq-box').addClass('loading-open');
+    $('.nav-number').addClass('loading-open');
+  })
 });
